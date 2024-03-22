9d108fa98c7c04242f274f51c6c5512e5f9065b7
Bilge.Adam <bilge.adam@baogrenci.com>
2024-03-22 09:50:28 +0300

    Initial commit

---
+++ src/week02/RealtionalOparetor.java
@@ -0,0 +1,7 @@
+package week02;
+
+public class RealtionalOparetor {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/week02/TypeCasting.java
@@ -0,0 +1,33 @@
+package week02;
+
+public class TypeCasting {
+    public static void main(String[] args) {
+        int dollar =5;
+        float dollarF = dollar;
+        long dollarL = dollar;
+
+        System.out.println("integer değeri:"+dollar
+                          + "floart değeri :"+dollarF
+                          + "Long değeri :" +dollarL);
+
+
+
+
+        float floatDollar = 13.5f;
+        int  intDollar = (int) floatDollar;
+
+
+
+
+         
+
+
+
+
+
+
+
+
+
+    }
+}
