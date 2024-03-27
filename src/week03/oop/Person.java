d7aa5ab4326f801b8a662236908f052383361d7a
aylincelikdemir <aylincelikdemir94@gmail.com>
2024-03-27 17:00:12 +0300

    file anlatıldı

---
+++ src/week03/oop/Person.java
@@ -0,0 +1,15 @@
+package week03.oop;
+
+public class Person {
+    String name;
+    String gender;
+
+    public void sleep(){
+        System.out.println(name+"is sleeping");
+
+
+    }
+    public void walk(){
+        System.out.println(name+ "is walkig");
+    }
+}
---
+++ src/week03/oop/Test.java
@@ -0,0 +1,23 @@
+package week03.oop;
+
+public class Test {
+    public static void main(String[] args) {
+        ClassName className = new ClassName();
+        System.out.println(className.sayHello("AYLİN"));
+        System.out.println(className.sum(1,5));
+
+        ClassName className2=new ClassName();
+        System.out.println(className2.sayHello("aylin"));
+        System.out.println(className.sum(8,12));
+
+
+        Person p1=new Person();
+        p1.name= "Aylin";
+        p1.gender="Aylin";
+
+
+
+    }
+
+
+}
